85690faedb7bbc0df51a8a662c4b024ee263a259
karthik9014 <pvpvkartheek99@gmail.com>
2021-09-15 16:33:58 +0530

    zoom

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express();
+const server = require("http").Server(app);
+
+app.get("/", function (req, res) {
+    res.redirect(`/${uuidv4()}`);
+});
+
+const port = process.env.PORT || 3030;
+
+const { v4: uuidv4 } = require("uuid");
+
+server.listen(port, function () {
+    console.log("server is running")
+});
